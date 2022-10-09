9c47e4a140e1c5776af13465049a7db2ba636600
astasovsky <stasovsky.ua@gmail.com>
2022-10-09 23:34:06 +0300

    Bonus features 2. Vertical player movement. Done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 
     private const float Speed = 10.0f;
     private const float XRange = 10;
+    private const float ZMin = -1.5f;
+    private const float ZMax = 15.5f;
     private float _horizontalInput;
+    private float _verticalInput;
 
 
     private void Update()
@@ -21,9 +24,20 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(XRange, transform.position.y, transform.position.z);
         }
+        
+        if(transform.position.z < ZMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, ZMin);
+        }
+        if(transform.position.z > ZMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, ZMax);
+        }
 
         _horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * (_horizontalInput * Time.deltaTime * Speed));
+        _verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * (_verticalInput * Time.deltaTime * Speed));
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
