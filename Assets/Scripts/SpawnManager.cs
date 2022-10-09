18562327bc9ce87dc76adccbbca6404a4c5fbb11
astasovsky <stasovsky.ua@gmail.com>
2022-10-09 17:02:16 +0300

    Task 2.3 Random Animal Stampede

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] animalPrefabs;
+
+    private const float SpawnRangeX = 10;
+    private const float SpawnPosZ = 20;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Randomly generate animal index and spawn position
+            Vector3 spawnPos = new(Random.Range(-SpawnRangeX, SpawnRangeX), 0, SpawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
